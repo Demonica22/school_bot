d59b02d50f9bc5ea19044c4bfb1f10bfd58415b1
Данил <GoodWin.228@yandex.ru>
2020-04-10 16:39:23 +0300

    изменена форма для добавления новостей

--- forms/add_news_form.py
+++ forms/add_news_form.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm, Form
-from wtforms import StringField, SubmitField, MultipleFileField
+from wtforms import StringField, SubmitField, MultipleFileField, TextAreaField
 from wtforms.validators import DataRequired
 
 class AddNewsForm(FlaskForm):
     title = StringField(validators=[DataRequired()])
-    data = StringField()
+    data = TextAreaField()
     files = MultipleFileField()
     submit = SubmitField()
